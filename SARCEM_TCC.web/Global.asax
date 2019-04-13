fa0188462d6d8dfb1b5e7355fb748fc9a2bedb70
Davidson Peixoto de Oliveira <dpeixoto.cc@gmail.com>
2019-04-13 17:02:08 -0300

    Add project files.

---
+++ SARCEM_TCC.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SARCEM_TCC.web.MvcApplication" Language="C#" %>
---
+++ SARCEM_TCC.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SARCEM_TCC.web.Startup))]
+namespace SARCEM_TCC.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
